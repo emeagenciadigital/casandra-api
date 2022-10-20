23eeaf7d3eced243d5c600083fb98d900627e1b0
Hugo Gutierrez <hugo.gutierrezm28@gmail.com>
2022-10-20 15:29:07 -0500

    fix

--- src/services/update-products/hooks/update-products.js
+++ src/services/update-products/hooks/update-products.js
@@ -1,12 +1,12 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 const { getItems, replaceItems } = require('feathers-hooks-common');
-const { NotFound, NotAcceptable } = require('@feathersjs/errors');
-const { parseInt } = require('lodash');
+// const { NotFound, NotAcceptable } = require('@feathersjs/errors');
+// const { parseInt } = require('lodash');
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
   return async (context) => {
-    const { user } = context.params;
+    // const { user } = context.params;
 
     const records = getItems(context);
 
