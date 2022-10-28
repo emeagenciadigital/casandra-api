2ee4a249ece44b48d414f409070998846155067e
Hugo Gutierrez <hugo.gutierrezm28@gmail.com>
2022-10-28 11:44:19 -0500

    fix

--- src/services/wallet-movements/wallet-movements.hooks.js
+++ src/services/wallet-movements/wallet-movements.hooks.js
@@ -2,25 +2,45 @@ const { iff, isProvider, discard } = require('feathers-hooks-common')
 
 module.exports = {
   before: {
-    all: [
-      iff(
-        isProvider('external'),
-        discard(
-          'created_by_user_id',
-          'payment_id',
-          'bonus_id',
-          'bonus_name',
-          'expired_day',
-          'expired_status',
-          'expired_wallet_movement_id',
-        )
-      )
-    ],
+    all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
+    create: [iff(
+      isProvider('external'),
+      discard(
+        'created_by_user_id',
+        'payment_id',
+        'bonus_id',
+        'bonus_name',
+        'expired_day',
+        'expired_status',
+        'expired_wallet_movement_id',
+      )
+    )],
+    update: [iff(
+      isProvider('external'),
+      discard(
+        'created_by_user_id',
+        'payment_id',
+        'bonus_id',
+        'bonus_name',
+        'expired_day',
+        'expired_status',
+        'expired_wallet_movement_id',
+      )
+    )],
+    patch: [iff(
+      isProvider('external'),
+      discard(
+        'created_by_user_id',
+        'payment_id',
+        'bonus_id',
+        'bonus_name',
+        'expired_day',
+        'expired_status',
+        'expired_wallet_movement_id',
+      )
+    )],
     remove: [],
   },
 
