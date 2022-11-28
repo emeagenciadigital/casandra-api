b0ea72ec736e54c5ab5069bcb104313305b70e98
Hugo Gutierrez <hugo.gutierrezm28@gmail.com>
2022-11-28 14:08:24 -0500

    fix

--- src/services/raw-queries/hooks/user-purchase-products.hook.js
+++ src/services/raw-queries/hooks/user-purchase-products.hook.js
@@ -9,15 +9,20 @@ const getUserPurchaseProducts = () => async (context) => {
   if (!user) throw new NotAcceptable('Required user login')
 
   const products = await stmt.query(
-    `select * from products where id in (select distinct orders_details.product_id
-      from orders_details
-               inner join orders on orders_details.order_id = orders.id
-          and orders.deletedAt is null
-          and orders.user_id = ${user.id}
-          and orders.order_status_id in (3, 5, 6, 7, 8, 9)
-      where orders_details.deletedAt is null)
-    and deletedAt is null
-    and status = 'active'`,
+    `select products.*, products_media.path main_image
+    from products
+             inner join products_media on products.id = products_media.product_id
+        and products_media.deletedAt is null
+    where products.id in (select distinct orders_details.product_id
+                          from orders_details
+                                   inner join orders on orders_details.order_id = orders.id
+                              and orders.deletedAt is null
+                              and orders.user_id = ${user.id}
+                              and orders.order_status_id in (3, 5, 6, 7, 8, 9)
+                          where orders_details.deletedAt is null)
+      and products.deletedAt is null
+      and products.status = 'active'
+    group by products.id`,
     { type: QueryTypes.SELECT }
   )
 
