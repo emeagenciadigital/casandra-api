4f46e408d61a9955bd97d0bed55975aa2eb44042
Hugo Gutierrez <hugo.gutierrezm28@gmail.com>
2022-12-03 09:33:57 -0500

    fix2

--- src/services/work-offers/hooks/sync-meilisearch.hook.js
+++ src/services/work-offers/hooks/sync-meilisearch.hook.js
@@ -1,9 +1,14 @@
 const { getItems } = require('feathers-hooks-common')
 
 const withAllData = (record) => async (context) => {
-  record.city = await context.app.service('locations-cities')
-    .getModel()
-    .findByPk(record.city_id)
+  [record.city, record.category] = await Promise.all([
+    context.app.service('locations-cities')
+      .getModel()
+      .findByPk(record.city_id),
+    context.app.service('work-offers-categories')
+      .getModel()
+      .findByPk(record.category_id)
+  ])
 
   return record
 }
