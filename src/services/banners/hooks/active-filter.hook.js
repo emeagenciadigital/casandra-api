6b88b02d6991b54ea0352f8d54bf394880b92415
Hugo Gutierrez <hugo.gutierrezm28@gmail.com>
2022-12-13 10:31:47 -0500

    fix2

--- src/services/banners/hooks/active-filter.hook.js
+++ src/services/banners/hooks/active-filter.hook.js
@@ -1,8 +1,11 @@
 const { QueryTypes } = require('sequelize')
+const moment = require('moment')
 
 module.exports = () => async (context) => {
   const query = context.params.query || {}
   const user = context.params.user
+
+  const now = moment().utcOffset(-5).format('YYYY-MM-DD H:mm:ss')
 
   if (!user || user?.role === 'user') {
     const stmt = context.app.get('sequelizeClient')
@@ -11,7 +14,7 @@ module.exports = () => async (context) => {
     from banners
     where status = 'active'
       and deletedAt is null
-      and if(type = 'FLASH_SALE', now() between start_date and end_date, true)
+      and if(type = 'FLASH_SALE', '${now}' between start_date and end_date, true)
     `, { type: QueryTypes.SELECT })
       .then(res => res.map(it => it.id))
     query.id = { $in: ids }
