f9552cae36ce257d7464c77ac3fa02a243d6fd9b
Hugo Gutierrez <hugo.gutierrezm28@gmail.com>
2022-12-19 16:04:27 -0500

    fix

--- src/services/user-course-chapter-views/hooks/calculate-progress.hook.js
+++ src/services/user-course-chapter-views/hooks/calculate-progress.hook.js
@@ -8,21 +8,23 @@ module.exports = () => async (context) => {
   const [chaptersViews, courseChapters] = await Promise.all([
     context.app.service('user-course-chapter-views')
       .getModel()
-      .count('id', {
+      .count({
+        col: 'id',
         where: {
           user_course_id: record.user_course_id
         }
       }),
     context.app.service('course-chapters')
       .getModel()
-      .count('id', {
+      .count({
+        col: 'id',
         where: {
           course_id: record.meta_course_id
         }
       })
   ])
 
-  const progress = (chaptersViews / courseChapters * 100).toFixed(0)
+  const progress = ((chaptersViews / courseChapters) * 100).toFixed(0)
 
   await context.app.service('user-courses')
     .getModel()
