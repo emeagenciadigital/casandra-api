28f7de5a7b62477688015dac1455fe9a27cabb5c
Hugo Gutierrez <hugo.gutierrezm28@gmail.com>
2022-12-19 15:28:19 -0500

    fix

--- src/services/wompi-webhook-events/hooks/webhook-event.hook.js
+++ src/services/wompi-webhook-events/hooks/webhook-event.hook.js
@@ -269,7 +269,7 @@ module.exports = () => async context => {
     const signature = record.signature
     const timestamp = record.timestamp
 
-    const delay = new Promise((resolve) => setTimeout(resolve, 5000))
+    const delay = () => new Promise((resolve) => setTimeout(resolve, 3000))
 
     await delay()
 
