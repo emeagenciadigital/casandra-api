34104b9bbd2cdc0b0bb545edec62fc55bac0299e
Hugo Gutierrez <hugo.gutierrezm28@gmail.com>
2022-12-30 08:54:01 -0500

    fix

--- src/services/process-payment-response/hooks/process-payment-orders.js
+++ src/services/process-payment-response/hooks/process-payment-orders.js
@@ -3,6 +3,7 @@
 const { getItems, replaceItems } = require('feathers-hooks-common');
 const registerOrderHistory = require('../../../hooks/register-order-history');
 const activateShoppingCart = require('../../../hooks/activate-shopping-cart');
+const { Op } = require('sequelize');
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
@@ -20,6 +21,101 @@ module.exports = (options = {}) => {
 
     let order_status_id = null
     let dataNotification = {}
+    let isOrderCompleted = false
+
+    if (
+      records.response_code === 'pending' ||
+      records.response_code === 'failure'
+    ) {
+      await activateShoppingCart({ order_id: order.id })(context);
+    } else if (
+      records.response_code === 'success' ||
+      records.response_code === 'approved'
+    ) {
+      const orderDetails = await context.app
+        .service('orders-details')
+        .getModel()
+        .query()
+        .where({ order_id: order.id, deletedAt: null });
+
+      const orderProducts = await context.app
+        .service('products')
+        .getModel()
+        .query()
+        .whereIn('id', orderDetails.map(it => it.product_id))
+        .where({ deletedAt: null })
+        .then(products => products.reduce((acc, it) => ({ ...acc, [it.id]: it})))
+
+      const productCoursesIds = orderProducts
+        .filter(product => product.course === 'true')
+        .map(product => product.id)
+      
+      let orderCourses = {}
+
+      if (productCoursesIds.length) {
+        orderCourses = await context.app
+          .service('courses')
+          .getModel()
+          .findAll({
+            where: {
+              product_id: {
+                [Op.in]: productCoursesIds
+              }
+            }
+        })
+        .then(courses => {
+          return courses.reduce((groupedCourses, course) => {
+            if (groupedCourses[course.product_id]) {
+              groupedCourses[course.product_id].push(course)
+            } else {
+              groupedCourses[course.product_id] = [course]
+            }
+            return groupedCourses
+          })
+        })
+
+      }
+
+       isOrderCompleted = orderProducts.every(product => product.course === 'true')
+
+      for (const key in orderDetails) {
+        const orderDetail = orderDetails[key];
+        const product = orderProducts[orderDetail.product_id]
+
+        // const product = await context.app
+        //   .service('products')
+        //   .getModel()
+        //   .query()
+        //   .where({ id: orderDetail.product_id })
+        //   .then(res => res[0])
+
+        if (product) {
+          if (product.course === 'false') {
+            await context.app
+              .service('products')
+              .getModel()
+              .query()
+              .where({ id: orderDetail.product_id })
+              .decrement('quantity', orderDetail.quantity);
+          } else {
+            const productCourses = orderCourses[product.id]
+            // const productCourses = await context.app
+            //   .service('courses')
+            //   .getModel()
+            //   .findAll({ where: { product_id: product.id, status: 'active' } })
+            await context.app
+              .service('user-courses')
+              .getModel()
+              .bulkCreate(
+                productCourses.map(course => ({
+                  user_id: order.user_id, course_id: course.id
+                }))
+              )
+          }
+        }
+
+      }
+    }
 
     switch (records.response_code) {
       //PAGO EXITOSO
@@ -42,7 +138,7 @@ module.exports = (options = {}) => {
 
           context.app.service('send-notifications').create(dataNotification),
         ]);
-        order_status_id = 3;
+        order_status_id = isOrderCompleted ? 9 :3;
 
         break;
 
@@ -84,58 +180,6 @@ module.exports = (options = {}) => {
         user_id: order.user_id,
       })(context),
     ]);
-
-    if (
-      records.response_code === 'pending' ||
-      records.response_code === 'failure'
-    ) {
-      await activateShoppingCart({ order_id: order.id })(context);
-    } else if (
-      records.response_code === 'success' ||
-      records.response_code === 'approved'
-    ) {
-      const orderDetails = await context.app
-        .service('orders-details')
-        .getModel()
-        .query()
-        .where({ order_id: order.id, deletedAt: null });
-
-      for (const key in orderDetails) {
-        const orderDetail = orderDetails[key];
-
-        const product = await context.app
-          .service('products')
-          .getModel()
-          .query()
-          .where({ id: orderDetail.product_id })
-          .then(res => res[0])
-
-        if (product) {
-          if (product.course === 'false') {
-            await context.app
-              .service('products')
-              .getModel()
-              .query()
-              .where({ id: orderDetail.product_id })
-              .decrement('quantity', orderDetail.quantity);
-          } else {
-            const productCourses = await context.app
-              .service('courses')
-              .getModel()
-              .findAll({ where: { product_id: product.id, status: 'active' } })
-            await context.app
-              .service('user-courses')
-              .getModel()
-              .bulkCreate(
-                productCourses.map(course => ({
-                  user_id: order.user_id, course_id: course.id
-                }))
-              )
-          }
-        }
-
-      }
-    }
 
     context.data = {
       order: order,
